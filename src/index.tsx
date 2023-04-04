cdd719f88535f858abdc65b577ea4a9db00c5241
Kingsley Etoka <etokakingsley@gmail.com>
2023-04-04 13:01:31 +0100

    setup sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/sass/main.scss';
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
