dcaf2f0b262f79b42273a2a720f02d2f61a1a042
Kingsley Etoka <etokakingsley@gmail.com>
2023-05-09 14:29:26 +0100

    footer first finish

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,28 +2,56 @@ import React from 'react'
 interface IGroup{
   title: string;
   isVisible: boolean;
-  content: string[]
+  content: string[];
+  isCommunity: boolean
 }
 const groups: IGroup[] = [
   {
     title: "About",
     isVisible: false,
-    content: new Array<string>(16).fill('Home')
+    content: new Array<string>(16).fill('About us'),
+    isCommunity: false,
   },
   {
-    title: "About",
+    title: "Products",
     isVisible: false,
-    content: new Array<string>(16).fill('Home')
+    content: new Array<string>(13).fill('product us'),
+    isCommunity: false,
   },
   {
-    title: "About",
+    title: "Service",
     isVisible: false,
-    content: new Array<string>(16).fill('Home')
+    content: new Array<string>(14).fill('service us'),
+    isCommunity: false,
   },
   {
-    title: "About",
+    title: "Support",
     isVisible: false,
-    content: new Array<string>(16).fill('Home')
+    content: new Array<string>(12).fill('support us'),
+    isCommunity: false,
+  },
+  {
+    title: "Learn",
+    isVisible: false,
+    content: new Array<string>(12).fill('learn us'),
+    isCommunity: false,
+  },
+  {
+    title: "Community",
+    isVisible: false,
+    content: [
+      'bxl-discord-alt',
+      'bxl-telegram',
+      'bxl-tiktok',
+      'bxl-facebook-circle',
+      'bxl-twitter',
+      'bxl-reddit',
+      'bxl-instagram',
+      'bxl-bitcoin',
+      'bx-envelope',
+      'bxl-youtube'
+    ],
+    isCommunity: true,
   }
 ];
 
@@ -32,19 +60,17 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <div className="grid">
-          <div className="footer__group">
+        <div className="footer__group">
 
-            {groups.map( grp => <FooterItem key={grp.title} {...grp} />)}
-            
-          </div>
+          {groups.map( (grp, index) => <FooterItem key={grp.title} index={index} {...grp} />)}
+          
         </div>
       </div>
     </footer>
   )
 }
 
-function FooterItem({content, title}: IGroup) {
+function FooterItem({content, title, isCommunity, index}: IGroup & {index: number}) {
 
   const [ isToggled, setToggle] = React.useState(false);
 
@@ -52,24 +78,32 @@ function FooterItem({content, title}: IGroup) {
 
   // }
   return (
-   <>
+   <div className={`${isCommunity ? 'cms' : index === 4 ? 'learn':''}`}>
      <div className='flex head'>
         <h3>{title}</h3>
-          <span onClick={() => setToggle(!isToggled)} >
-            <i className={`bx ${isToggled ? 'bx-minus' : 'bx-plus'}`}></i>
+          {
+            !isCommunity && <span  onClick={() => setToggle(!isToggled)} >
+            <i className={`bx ${isToggled ? 'bx-minus' : 'bx-plus'} ich`}></i>
           </span>
+          }
       </div>
-    <div className="gp">
+
       {
-        isToggled && <ul >
+        <ul className={isCommunity ? 'grid community': isToggled ? 'show-accord': 'hide-accord'}>
         {
-          content.map((item, i) => <li key={i}><a href="/">{item}</a></li>)
+          content.map((item, i) =>{
+            const defaultList =  <li><a href="/">{item}</a></li>
+            return <>
+              {
+                isCommunity ? <li key={i}><i className={`bx ${item}`}></i></li> :  defaultList
+              }
+            </>
+          })
         }
-      </ul>
+        </ul>
       }
-    </div>
       
-   </>
+   </div>
     
   )
 }
