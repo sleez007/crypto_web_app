7b9a2ab946041575cd19f5d9746b8b9f8a8943f4
Kingsley Etoka <etokakingsley@gmail.com>
2023-05-16 14:24:48 +0100

    klhggkgkjhgkl

--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -3,7 +3,29 @@ import React from 'react'
 export default function Dashboard() {
   return (
     <div>
-        Dashboard Index
+      <div className="d-main grid">
+        <div className="d-main__content">
+          <div className="summary flex">
+            {
+              new Array(3).fill('').map(
+                e => (
+                  <div className="summary__card">
+                    <div>
+                      <p>Total Revenue</p>
+                      <span></span>
+                    </div>
+                    <div>
+                      <p>$4.00</p>
+                      <span>+6.2%</span>
+                    </div>
+                  </div>
+                )
+              )
+            }
+          </div>
+        </div>
+        <aside className="d-main__aside">kbjb</aside>
+      </div>
     </div>
   )
 }
--- src/layout/dashboard_layout.tsx
+++ src/layout/dashboard_layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Appbar from './appbar'
 import { Outlet } from 'react-router-dom'
+import Sidebar from './sidebar'
 
 export default function Dashboardlayout() {
   return (
     <section className="dashboard">
-        <aside className="dashboard__sidebar"></aside>
+        <Sidebar />
         <main className="dashboard__layout">
             <Appbar />
             <Outlet />
